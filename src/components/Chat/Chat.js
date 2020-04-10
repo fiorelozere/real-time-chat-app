8e360fa4b42d7d34171a0d8d25add02dae8ee81f
fiorelozere <47506023+fiorelozere@users.noreply.github.com>
2020-04-10 16:23:58 +0200

    update

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
+
+import TextContainer from '../TextContainer/TextContainer';
+import Messages from '../Messages/Messages';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+
 import './Chat.css';
 
 let socket;
@@ -9,28 +14,35 @@ let socket;
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'localhost:5000';
+  const ENDPOINT = 'https://nodereactchat.herokuapp.com/';
+
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
+
     socket = io(ENDPOINT);
-    setName(name);
+
     setRoom(room);
+    setName(name);
 
-    socket.emit('join', { name, room });
-
-    return () => {
-      socket.emit('disconnect');
-      socket.off();
-    };
+    socket.emit('join', { name, room }, (error) => {
+      if (error) {
+        alert(error);
+      }
+    });
   }, [ENDPOINT, location.search]);
 
   useEffect(() => {
     socket.on('message', (message) => {
-      setMessages([...messages, message]);
+      setMessages((messages) => [...messages, message]);
     });
-  }, [messages]);
+
+    socket.on('roomData', ({ users }) => {
+      setUsers(users);
+    });
+  }, []);
 
   const sendMessage = (event) => {
     event.preventDefault();
@@ -40,17 +52,14 @@ const Chat = ({ location }) => {
     }
   };
 
-  console.log(message, messages);
-
   return (
     <div className="outerContainer">
       <div className="container">
-        <input
-          value={message}
-          onChange={(event) => setMessage(event.target.value)}
-          onKeyPress={(event) => (event.key === 'Enter' ? sendMessage(event) : null)}
-        />
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
+      <TextContainer users={users} />
     </div>
   );
 };
